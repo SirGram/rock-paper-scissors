f3975a9ea65e36a924dec7ef8667ab5a774ce0e1
sirgram <gabrieulogio@gmail.com>
2023-12-11 19:43:52 +0100

    revisiting paper-scissors

--- game.js
+++ game.js
@@ -1,49 +1,86 @@
 function getComputerChoice() {
-    const randomNumber= Math.floor(Math.random() * options.length);
-    return options[randomNumber]
+  const randomNumber = Math.floor(Math.random() * options.length);
+  return options[randomNumber];
 }
-function result(pcChoice,userChoice){
-    if (pcChoice === userChoice) {
-        console.log("It's a tie!");
-      } else if (
-        (pcChoice === "P" && userChoice === "R") ||
-        (pcChoice === "R" && userChoice === "S") ||
-        (pcChoice === "S" && userChoice === "P")
-      ) {
-        console.log("PC wins!");
-        ++pcCounter;
-      } else {
-        console.log("Player wins!");
-        ++playerCounter;
-      }
+
+function result(pcChoice, userChoice) {
+  if (pcChoice === userChoice) {
+      console.log("It's a tie!");
+  } else if (
+      (pcChoice === "P" && userChoice === "R") ||
+      (pcChoice === "R" && userChoice === "S") ||
+      (pcChoice === "S" && userChoice === "P")
+  ) {
+      console.log("PC wins!");
+      ++pcCounter;
+  } else {
+      console.log("Player wins!");
+      ++playerCounter;
+  }
 }
-function endresult(){
+let playerCounter = 0;
+let pcCounter = 0;
+const options = ["R", "P", "S"];
+let userChoice;
 
+const score = document.createElement("h2");
+score.classList.add("score");
+let scoreContainer;
+
+function playRound() {
+  const pcChoice = getComputerChoice();
+  result(pcChoice, userChoice);
+
+  score.textContent = "Player " + playerCounter + " / " + pcCounter + " PC";
+  if (playerCounter >= 5 || pcCounter >= 5) {
+    announceWinner();
+    gameOver = true;
+  }
+
+  return !gameOver;
 }
-const numRounds=5;
-let currentRound=1;
-let playerCounter=0;
-let pcCounter=0;
-const options = ["S", "P", "R"];
 
-while(currentRound<=numRounds){
-    console.log("Round: "+ currentRound)
-    const userChoice = prompt("Select your input. S Scissors, P paper, R rock!").toUpperCase();
-
-    if (!options.includes(userChoice)){
-        console.log("Incorrect input");
-    }
-    else{
-        console.log("Player chose: " + userChoice)
-
-        const pcChoice= getComputerChoice();
-        console.log("PC chose: " + pcChoice);
-
-        result(pcChoice, userChoice);
-        currentRound++;
-        console.log("Player " + playerCounter + " / " + pcCounter + " PC")
-        
-    }
+function announceWinner(){
+  const scoreResult = document.createElement("h3")
+  if (playerCounter> pcCounter){
+    scoreResult.textContent = "PLAYER WINS"
+  }
+  else { scoreResult.textContent = "PLAYER LOSES"}
+  score.innerHTML = "";
+  score.appendChild(scoreResult)
 }
+
+function handleChoice() {
+  userChoice = this.name;
+  if (playRound(userChoice)){    
+  }
+  else{
+    announceWinner();
+    disableButtons();
+  }
+}
+function disableButtons() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.removeEventListener("click", handleChoice); // Remove the click event listener
+    button.disabled = true; // Disable the button
+  });
+}
+let gameOver = false
+document.addEventListener("DOMContentLoaded", function () {
+  scoreContainer = document.querySelector(".score-container");
+
+  const buttons = document.querySelectorAll("button");
+  if (!gameOver){
+    buttons.forEach(button => button.addEventListener("click", handleChoice));
+  }
+  
+  score.textContent = "Player " + playerCounter + " / " + pcCounter + " PC";
+
+  if (scoreContainer) {
+    scoreContainer.appendChild(score);
+  }
+
+});
 
 
